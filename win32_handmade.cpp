19351da14a192927c907d9dfc3299c3b79e45416
Donovan Anderson <Donovan.Anderson@ey.com>
2022-03-24 09:11:52 -0400

    Added sine wave output

--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -1,9 +1,9 @@
-#define UNICODE
 #include<windows.h>
 #include<stdint.h>
 #include<Xinput.h>
 #include<dsound.h>
 #include<stdio.h>
+#include<cmath>
 
 #ifndef global_variable
 #define global_variable static
@@ -50,6 +50,22 @@ using uint8 = uint8_t;
 using uint32 = uint32_t;
 using int32 = int32_t;
 using int16 = int16_t;
+using real32 = float;
+using real64 = double;
+
+const real32 Pi32 = 3.14159265359;
+
+struct win32_sound_output
+{
+    int16 tone_volume = 3000;
+    int32 samples_per_second = 48000;
+    int32 bytes_per_sample = sizeof(int16)*2;
+    int32 Hz = 256;
+    uint32 wave_period = samples_per_second/Hz;
+    uint32 running_sample_index = 0;
+    uint32 buffer_size = samples_per_second * sizeof(int16)*2;
+    real32 tSine = 0;
+};
 
 //Stubs for XInput State Windows functions.  This allows use program to not have to use xbox controllers when it isn't available.  Input can 
 //be taken from the keyboard
@@ -73,7 +89,7 @@ global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
 internal LPDIRECTSOUNDBUFFER
 Win32InitDSound(HWND Window, int32 SamplesPerSecond, int32 BufferSize)
 {
-    auto DirectSoundLibrary = LoadLibrary(L"dsound.dll");
+    auto DirectSoundLibrary = LoadLibraryA("dsound.dll");
     LPDIRECTSOUNDBUFFER SecondaryBuffer = NULL;
     if(DirectSoundLibrary)
     {
@@ -138,13 +154,12 @@ Win32InitDSound(HWND Window, int32 SamplesPerSecond, int32 BufferSize)
 internal void
 Win32XLoadInput()
 {
-    auto XInputLdLbry = LoadLibrary(L"x_input1_4.dll");
+    auto XInputLdLbry = LoadLibraryA("x_input1_4.dll");
     if(!XInputLdLbry)
-        XInputLdLbry =  LoadLibrary(L"x_input1_3.dll");
+        XInputLdLbry =  LoadLibraryA("x_input1_3.dll");
 
     XInputGetState = reinterpret_cast<x_input_get_state*>(GetProcAddress(XInputLdLbry,"XInputGetState"));
     XInputSetState = reinterpret_cast<x_input_set_state*>(GetProcAddress(XInputLdLbry,"XInputSetState"));
-    
 }
 
 internal void
@@ -312,12 +327,12 @@ LRESULT CALLBACK WindowProc(HWND Window,
     return result;
 }
 
-void Win32WriteSquareWaveToBuffer(LPDIRECTSOUNDBUFFER sound_buffer, DWORD buffer_size)
+void Win32WriteSquareWaveToBuffer(LPDIRECTSOUNDBUFFER sound_buffer, DWORD buffer_size, uint32& running_sample_index)
 {
     int Hz = 256, samples_per_second = 48000, bytes_per_sample = sizeof(int16)*2;
-    int square_wave_period = samples_per_second/Hz;
-    int half_square_wave_period = square_wave_period / 2;
-    internal uint32 running_sample_index = 0;
+    uint32 wave_period = samples_per_second/Hz;
+    uint32 half_wave_period = wave_period / 2; 
+    
     DWORD write_cursor, play_cursor;
     LPVOID region_1, region_2;
     DWORD region_1_size, region_2_size, bytes_to_write;
@@ -343,7 +358,7 @@ void Win32WriteSquareWaveToBuffer(LPDIRECTSOUNDBUFFER sound_buffer, DWORD buffer
             
             for(DWORD sample_index = 0; sample_index < region_1_sample_count; ++sample_index)
             {
-                int16 sample_value = (running_sample_index > (square_wave_period/2)) ? 16000 : -16000;
+                int16 sample_value = (running_sample_index / half_wave_period) % 2 ? 16000 : -16000;
                 *sample++ = sample_value;//write to left
                 *sample++ = sample_value;//write to right
                 ++running_sample_index;
@@ -352,14 +367,66 @@ void Win32WriteSquareWaveToBuffer(LPDIRECTSOUNDBUFFER sound_buffer, DWORD buffer
             sample = reinterpret_cast<int16*>(region_2);
             for(DWORD sample_index = 0; sample_index < region_2_sample_count; ++sample_index)
             {
-                int16 sample_value = ((running_sample_index / half_square_wave_period) % 2) ? 16000 : -16000;
+                int16 sample_value = ((running_sample_index / half_wave_period) % 2) ? 3000 : -3000;
                 *sample++ = sample_value;//write to left
                 *sample++ = sample_value;//write to right
                 ++running_sample_index;
             }
             
             //unlock the secondary buffer
-           // sound_buffer->Unlock(write_cursor,bytes_to_write,)
+            sound_buffer->Unlock(region_1,region_1_size,region_2,region_2_size);
+        }        
+    }
+}
+
+void Win32WriteSineWaveToBuffer(LPDIRECTSOUNDBUFFER sound_buffer, win32_sound_output &sound_output)
+{
+    DWORD write_cursor, play_cursor;
+    LPVOID region_1, region_2;
+    DWORD region_1_size, region_2_size, bytes_to_write;
+
+    if(SUCCEEDED(sound_buffer->GetCurrentPosition(&play_cursor,&write_cursor)))    
+    {
+        DWORD byte_to_lock = (sound_output.running_sample_index * sound_output.bytes_per_sample) % sound_output.buffer_size;
+        
+        if(byte_to_lock > play_cursor)
+        {
+            bytes_to_write = sound_output.buffer_size - byte_to_lock;
+            bytes_to_write += play_cursor;
+        }
+        else
+        {
+            bytes_to_write = play_cursor - byte_to_lock;
+        }
+
+        if(SUCCEEDED(sound_buffer->Lock(write_cursor,bytes_to_write, &region_1,&region_1_size,&region_2,&region_2_size,0)))
+        {
+            DWORD region_1_sample_count = region_1_size / sound_output.bytes_per_sample, region_2_sample_count = region_2_size / sound_output.bytes_per_sample;
+            auto sample = reinterpret_cast<int16*>(region_1);
+            
+            for(DWORD sample_index = 0; sample_index < region_1_sample_count; ++sample_index)
+            {
+                real32 sine_value = sin(sound_output.tSine);
+                int16 sample_value = (int16)(sine_value * sound_output.tone_volume);
+                *sample++ = sample_value;//write to left
+                *sample++ = sample_value;//write to right
+                sound_output.tSine += ((real32)2.0 * Pi32) / (real32)sound_output.wave_period;
+                ++sound_output.running_sample_index; 
+            }
+            
+            sample = reinterpret_cast<int16*>(region_2);
+            for(DWORD sample_index = 0; sample_index < region_2_sample_count; ++sample_index)
+            {
+                real32 sine_value = sin(sound_output.tSine);
+                int16 sample_value = (int16)(sine_value * sound_output.tone_volume);
+                *sample++ = sample_value;//write to left
+                *sample++ = sample_value;//write to right
+                sound_output.tSine += ((real32)2.0 * Pi32) / (real32)sound_output.wave_period;
+                ++sound_output.running_sample_index;
+            }
+            
+            //unlock the secondary buffer
+            sound_buffer->Unlock(region_1,region_1_size,region_2,region_2_size);
         }        
     }
 }
@@ -371,23 +438,21 @@ int WINAPI WinMain(
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-    WNDCLASS WindowClass = {};
+    WNDCLASSA WindowClass = {};
 
 	Win32ResizeDIBSection(global_back_buffer,1280,720);
 
     WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
     WindowClass.lpfnWndProc = &WindowProc;
     WindowClass.hInstance = hInstance;
-    LPCWSTR className = L"HandmadeHeroWindowClass";
+    LPCSTR className = "HandmadeHeroWindowClass", window_name="Handmade Hero";
     WindowClass.lpszClassName = className;
-    LPCWSTR windowName = L"Handmade Hero";
 
-    if(RegisterClass(&WindowClass))
+    if(RegisterClassA(&WindowClass))
     {
-        HWND WindowHandle = CreateWindowEx(
-            0,
+        HWND WindowHandle = CreateWindowA(
             className,
-            windowName,
+            window_name,
             WS_OVERLAPPEDWINDOW|WS_VISIBLE,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
@@ -402,13 +467,19 @@ int WINAPI WinMain(
         if(WindowHandle != NULL)
         {
             int xOffset = 0, yOffset = 0;
-            int32 buffer_size = 48000 * sizeof(int16)*2;
-            auto sound_buffer = Win32InitDSound(WindowHandle, 48000, buffer_size);
+            
+            //sound test variables and setup
+            int32 samples_per_second = 48000;
+            uint32 running_sample_index = 0, buffer_size = samples_per_second * sizeof(int16)*2;
+            auto sound_buffer = Win32InitDSound(WindowHandle, samples_per_second, buffer_size);
+            win32_sound_output sound_output{};
+            //Win32WriteSquareWaveToBuffer(sound_buffer,buffer_size, running_sample_index);
+            Win32WriteSineWaveToBuffer(sound_buffer, sound_output);
+            sound_buffer->Play(0,0,DSBPLAY_LOOPING);
 
             MSG Message;
             HDC deviceContext = GetDC(WindowHandle);
             XINPUT_STATE ControllerState;            
-            sound_buffer->Play(0,0,DSBPLAY_LOOPING);
             while(Running)
             {
                 while(PeekMessage(&Message,WindowHandle,0,0,PM_REMOVE))
@@ -441,17 +512,21 @@ int WINAPI WinMain(
 
                         int16 StickX = Pad->sThumbLX;
                         int16 StickY = Pad->sThumbRX;
+
+                        //joke match sound to xbox controller stick
+                        sound_output.Hz = 512 + (int32)(256.0f*((real32)StickY / 30000.0f));
+                        sound_output.wave_period = sound_output.samples_per_second/sound_output.Hz;
                     }
                 }
 
 				RenderWeirdGradient(global_back_buffer,xOffset++,yOffset++);
-                //sound output test
-                Win32WriteSquareWaveToBuffer(sound_buffer,buffer_size);
                 
+                //sound output test
+                //Win32WriteSquareWaveToBuffer(sound_buffer,buffer_size, running_sample_index);
+                Win32WriteSineWaveToBuffer(sound_buffer,sound_output);
 
-				win32_window_dimension wd = get_window_dimension(WindowHandle);
-				Win32CopyBufferToWindow(deviceContext, wd.Width,wd.Height,
-										global_back_buffer);
+                win32_window_dimension wd = get_window_dimension(WindowHandle);
+				Win32CopyBufferToWindow(deviceContext, wd.Width,wd.Height,global_back_buffer);
             }
 			ReleaseDC(WindowHandle,deviceContext);
         }
